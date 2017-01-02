f83560fc967a0d860b12de80c1894495d0d2d165
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2017-01-02 18:37:12 +0530

    jquery displaying the notes

--- views/js/script.js
+++ views/js/script.js
@@ -1,12 +1,35 @@
 $(document).ready(function(){
 
-    $.get("readall",function (data,status) {
-        alert("Data: " + data + "\nStatus " + status);
+    // $.get("readall",function (data,status) {
+    //     alert("Data: " + data + "\nStatus " + status);
+    //   });
+
+    $.get("readall",function (arrayOfNotes) {
+        //alert("Data: " + data);
+        console.log(arrayOfNotes);
+        console.log(arrayOfNotes.length);
+
+        var newHtml = [];
+
+        for(var i=0;i<arrayOfNotes.length;i++){
+            newHtml.push('<span><b>' + arrayOfNotes[i].title + '</b></span><br><span>'+ arrayOfNotes[i].body +'</span><br>');
+        };
+
+        $(".all_notes_div").html(newHtml.join(""));
+        
+        // $.each(arrayOfNotes,function(index,value){
+        //     $('.all_notes_div').html( $('.element').html() + '<span>' + '2' + '</span>');
+        // });
+
+
+         // $(".all_notes_div").text(data);    
+
+      }).fail(function(jqXHR,textStatus,errorThrown){
+          //alert('woops' + textStatus + errorThrown);
+          $(".all_notes_div").text(errorThrown);
       });
+
 });
 
-
-// 1. in this script try to load the error in the alert 
-// the error is when the mongodb server is not connected
 
 // 2. when that error is solved, use mongod.exe to start db and then check to connect
