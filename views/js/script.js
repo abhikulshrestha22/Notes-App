985ea3842a5044db5e69b588a268d4cc9d03cc66
Abhishek Kulshrestha <abhikulshrestha22@gmail.com>
2017-01-03 20:18:05 +0530

    delete option added

--- views/js/script.js
+++ views/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
+    readAllNotes();
 
+});
+
+
+function readAllNotes(){
     $.get("readall",function (arrayOfNotes) {
 
         console.log(arrayOfNotes);
@@ -9,7 +14,7 @@ $(document).ready(function(){
         var newHtml = [];
 
         for(var i=arrayOfNotes.length;i>0;i--){
-            newHtml.push('<div class = "note_div container"><div class="row"><div class="col-xs-2">'+ (arrayOfNotes.length-i+1) +'</div><div class="col-xs-4"><h4>'+ arrayOfNotes[i-1].title+'</h4></div><div class="col-xs-6">'+ arrayOfNotes[i-1].body +'</div></div></div>');
+            newHtml.push('<div class = "note_div container"><div class="row'+ (arrayOfNotes.length-i+1)+'"><div class="col-xs-2">'+ (arrayOfNotes.length-i+1) +'</div><div class="col-xs-3"><h4>'+ arrayOfNotes[i-1].title+'</h4></div><div class="col-xs-5">'+ arrayOfNotes[i-1].body +'</div><div class="col-xs-2"><button type="button" class="btn btn-default btn-primary" onclick="delbtnclick(this.id);" id="delbtn'+ (arrayOfNotes.length-i+1) +'">Delete</button></div></div></div>');
  
     };
 
@@ -20,9 +25,7 @@ $(document).ready(function(){
           //alert('woops' + textStatus + errorThrown);
           $(".all_notes_div").text(errorThrown);
       });
-
-});
-
+}
 
 function writeNote(){
     var title = $('#title').val();
@@ -41,5 +44,30 @@ function writeNote(){
     },"json").fail(function(jqXHR,textStatus,errorThrown){
           console.log(errorThrown); 
       });
+
+}
+
+function delbtnclick(id){
+
+numId = id.replace(/^\D+/g,'');
+
+var i=numId;
+var title = $(`.row${i}`).find('.col-xs-3').text();
+var body = $(`.row${i}`).find('.col-xs-5').text();
+
+    $.post("delete",{
+            "title":title,
+            "body":body
+        }, function(data){
+
+            console.log(data);
+            if(data.n = 1 || data.ok ==1){
+                console.log("removed");
+                readAllNotes();
+            }
+        },"json").fail(function(jqXHR,textStatus,errorThrown){
+            console.log(errorThrown); 
+        });
+
 
 }
